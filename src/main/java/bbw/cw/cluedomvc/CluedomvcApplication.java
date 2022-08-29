e5cf8dbd6699d2998ac2136ae20b0c2fc546cff1
Cameron Weijmer <weijmer.c@gmail.com>
2022-08-29 16:35:19 +0200

    Setup of Project

---
+++ src/main/java/bbw/cw/cluedomvc/CluedomvcApplication.java
@@ -0,0 +1,13 @@
+package bbw.cw.cluedomvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CluedomvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CluedomvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bbw/cw/cluedomvc/CluedomvcApplicationTests.java
@@ -0,0 +1,13 @@
+package bbw.cw.cluedomvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CluedomvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
