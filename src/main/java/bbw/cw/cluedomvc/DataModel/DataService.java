573ac278826403ea81908b9d7591e57ef881e005
Cameron Weijmer <weijmer.c@gmail.com>
2022-08-29 17:41:35 +0200

    Added persons to the Dataservice as List

--- src/main/java/bbw/cw/cluedomvc/DataModel/DataService.java
+++ src/main/java/bbw/cw/cluedomvc/DataModel/DataService.java
@@ -1,11 +1,18 @@
 package bbw.cw.cluedomvc.DataModel;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class DataService {
 
     private List<Person>personList = new ArrayList<>(
-            
-    );
+            Arrays.asList(
+                    new Person("scarlett", "Miss", "25", "blonde", "red"),
+                    new Person("Green", "rev", "80", "grey", "green"),
+                    new Person("Mustard", "Colonel", "85", "grey", "brown"),
+                    new Person("Plum", "Professor", "70", "Black", "violett"),
+                    new Person("Peacock", "Misses", "50", "Brown", "Blue"),
+                    new Person("White", "Misses", "70", "grey", "White")));
+
 }
