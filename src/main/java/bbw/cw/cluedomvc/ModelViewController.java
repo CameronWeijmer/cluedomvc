a3aee7e41a265a4b7fc014fb455af81d32d95ad0
Cameron Weijmer <weijmer.c@gmail.com>
2022-08-29 22:04:10 +0200

    Added Modelview-Controller for the Personlist

---
+++ src/main/java/bbw/cw/cluedomvc/ModelViewController.java
@@ -0,0 +1,23 @@
+package bbw.cw.cluedomvc;
+
+/*
+*   ModelViewController
+*
+* @author Cameron Weijmer
+* @version 29.08.2022
+*
+* */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ModelViewController {
+
+    @GetMapping("/showCriminals"){
+        public String showCriminals(){
+            System.out.println("ModelViewController.showCriminals");
+            return "personview";
+        }
+    }
+}
