0ed5bb103fcab9ce9f6fc52542fdcdf10e544412
Cameron Weijmer <weijmer.c@gmail.com>
2022-08-29 17:24:51 +0200

    Added Data service

---
+++ src/main/java/bbw/cw/cluedomvc/DataModel/DataService.java
@@ -0,0 +1,11 @@
+package bbw.cw.cluedomvc.DataModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataService {
+
+    private List<Person>personList = new ArrayList<>(
+            
+    );
+}
---
+++ src/main/java/bbw/cw/cluedomvc/DataModel/Person.java
@@ -0,0 +1,58 @@
+package bbw.cw.cluedomvc.DataModel;
+
+public class Person {
+
+    private String name;
+    private String formOfAddress;
+    private String age;
+    private String hairColor;
+    private String clothColor;
+
+    public Person(String name, String formOfAddress, String age, String hairColor, String clothColor) {
+        this.name = name;
+        this.formOfAddress = formOfAddress;
+        this.age = age;
+        this.hairColor = hairColor;
+        this.clothColor = clothColor;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFormOfAddress() {
+        return formOfAddress;
+    }
+
+    public void setFormOfAddress(String formOfAddress) {
+        this.formOfAddress = formOfAddress;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getHairColor() {
+        return hairColor;
+    }
+
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+    }
+
+    public String getClothColor() {
+        return clothColor;
+    }
+
+    public void setClothColor(String clothColor) {
+        this.clothColor = clothColor;
+    }
+}
