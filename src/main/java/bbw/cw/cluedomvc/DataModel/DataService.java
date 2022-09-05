0027e76eb2d166c713885d84440c76d600b14bdb
Cameron Weijmer <weijmer.c@gmail.com>
2022-09-05 16:56:03 +0200

    Fixed Syntax error

--- src/main/java/bbw/cw/cluedomvc/DataModel/DataService.java
+++ src/main/java/bbw/cw/cluedomvc/DataModel/DataService.java
@@ -1,9 +1,12 @@
 package bbw.cw.cluedomvc.DataModel;
 
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@Service
 public class DataService {
 
     private List<Person> personList = new ArrayList<>(
@@ -14,7 +17,6 @@ public class DataService {
                     new Person("Plum", "Professor", "70", "Black", "violett"),
                     new Person("Peacock", "Misses", "50", "Brown", "Blue"),
                     new Person("White", "Misses", "70", "grey", "White")));
-
     public List<Person> getPersonList() {
         return personList;
     }
--- src/main/java/bbw/cw/cluedomvc/ModelViewController.java
+++ src/main/java/bbw/cw/cluedomvc/ModelViewController.java
@@ -1,21 +1,19 @@
 package bbw.cw.cluedomvc;
 
 /*
-*   ModelViewController
-*
-* @author Cameron Weijmer
-* @version 04.09.2022
-*
-* */
+ *   ModelViewController
+ *
+ * @author Cameron Weijmer
+ * @version 04.09.2022
+ *
+ * */
 
 import bbw.cw.cluedomvc.DataModel.DataService;
 import bbw.cw.cluedomvc.DataModel.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ModelViewController {
@@ -23,11 +21,19 @@ public class ModelViewController {
     @Autowired
     private DataService list;
 
-    @RequestMapping(value = {"/", "/addPerson"}, method = RequestMethod.GET)
-    public String showCrowdfunding(Model model) {
-        model.addAttribute("item", new Person());
-        model.addAttribute("Persons", list.getPersonList());
-        System.out.println("ModelViewController.showPerson");
+    @GetMapping(value = {"/", "/addPerson"})
+    public String showPersons(Model model) {
+        model.addAttribute("Person", list.getPersonList());
+        System.out.println("ModelViewController.showPersons");
         return "personview";
     }
+
+//    @PostMapping("/addPerson")
+//    public String addDonation(@ModelAttribute Person personList, Model model) {
+//        list.getPersonList().add(personList);
+//        model.addAttribute("item", new Person());
+//        model.addAttribute("donations", list.getPersonList());
+//        System.out.println("ViewController.person");
+//        return "personview";
+//    }
 }
